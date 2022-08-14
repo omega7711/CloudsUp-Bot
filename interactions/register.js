4bd2798dac452d8f4a8d35e4467b6ceafe36beaa
omega7711 <arthursensique77@gmail.com>
2022-08-14 02:56:01 +0200

    http install

--- interactions/register.js
+++ interactions/register.js
@@ -6,13 +6,16 @@ const RegisterCommands = (config, token) => {
             .setName('link')
             .setDescription('Vous connecte à votre compte Minecraft!')
             .addStringOption(option =>
-            option.setName('input')
-                .setDescription('The input to echo back')
+            option.setName('pseudo')
+                .setDescription('Le pseudo du joueur à synchroniser!')
                 .setRequired(true)),
     ]
         .map(command => command.toJSON());
     
-    const rest = new REST({ version: '10' }).setToken(config.token);
+    const rest = new REST({ version: '10' }).setToken(token);
+    rest.delete(Routes.applicationCommand(config.clientId, 'commandId'))
+	.then(() => console.log('Successfully deleted application command'))
+	.catch(console.error);
     
     rest.put(Routes.applicationGuildCommands(config.clientId, config.guildId), { body: commands })
         .then(() => console.log('Successfully registered application commands.'))
